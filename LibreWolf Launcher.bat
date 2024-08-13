ac289e4ea0229ce083a06f6676b215b9808f9fd1
Enobraed <70288107+Enobraed@users.noreply.github.com>
2024-08-13 18:11:06 +0300

    Add files via upload

--- LibreWolf Launcher.bat
+++ LibreWolf Launcher.bat
@@ -1,18 +1,26 @@
 @echo off
 taskkill /f /im "librewolf.exe" >nul 2>&1
+del "%~dp0\Profiles\Default\activity-stream.weather_feed.json" >nul 2>&1
 del "%~dp0\Profiles\Default\cert_override.txt" >nul 2>&1
 del "%~dp0\Profiles\Default\cookies.sqlite-wal" >nul 2>&1
+del "%~dp0\Profiles\Default\enumerate_devices.txt" >nul 2>&1
 del "%~dp0\Profiles\Default\extensions.json.pbak" >nul 2>&1
+del "%~dp0\Profiles\Default\favicons.sqlite" >nul 2>&1
+del "%~dp0\Profiles\Default\favicons.sqlite-shm" >nul 2>&1
 del "%~dp0\Profiles\Default\favicons.sqlite-wal" >nul 2>&1
+del "%~dp0\Profiles\Default\formhistory.sqlite" >nul 2>&1
 del "%~dp0\Profiles\Default\notificationstore.json" >nul 2>&1
 del "%~dp0\Profiles\Default\parent.lock" >nul 2>&1
 del "%~dp0\Profiles\Default\pkcs11.txt.pbak" >nul 2>&1
 del "%~dp0\Profiles\Default\places.sqlite-wal" >nul 2>&1
 del "%~dp0\Profiles\Default\prefs.js.pbak" >nul 2>&1
+del "%~dp0\Profiles\Default\serviceworker.txt" >nul 2>&1
+del "%~dp0\Profiles\Default\times.json" >nul 2>&1
 del "%~dp0\Profiles\Default\webappsstore.sqlite-wal" >nul 2>&1
 rd /s /q "%~dp0\Profiles\Default\bookmarkbackups" >nul 2>&1
 rd /s /q "%~dp0\Profiles\Default\cache2" >nul 2>&1
 rd /s /q "%~dp0\Profiles\Default\datareporting" >nul 2>&1
+rd /s /q "%~dp0\Profiles\Default\extensions\trash" >nul 2>&1
 rd /s /q "%~dp0\Profiles\Default\security_state" >nul 2>&1
 rd /s /q "%~dp0\Profiles\Default\settings" >nul 2>&1
 rd /s /q "%~dp0\Profiles\Default\shader-cache" >nul 2>&1
